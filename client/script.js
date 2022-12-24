62798d2fd8902ec43f9ecee8c034c0b23f7e2871
raj99gaurav <rajgaurav.mail@gmail.com>
2022-12-24 15:23:56 +0530

    function to create unique ids to  map to msgs

--- client/script.js
+++ client/script.js
@@ -35,3 +35,13 @@ function typeText(element, text) {
     }
   }, 20);
 }
+
+//function to create unique ids to  map to msgs
+function generateUniqueId() {
+  //using current time and date
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
